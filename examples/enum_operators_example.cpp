5c87fc7d6c8ab4c16b3d45c9fd41f7de466246dd
Lucas Vocanson <lucas.vocanson@gmail.com>
2025-04-01 18:22:06 +0200

    spc to tabs for coherence

--- examples/enum_operators_example.cpp
+++ examples/enum_operators_example.cpp
@@ -3,54 +3,54 @@
 
 enum class SomeFlag : uint8_t
 {
-    OptionA = 1 << 0,
-    OptionB = 1 << 1,
-    OptionC = 1 << 2,
-    OptionD = 1 << 3,
-    OptionE = 1 << 4,
+	OptionA = 1 << 0,
+	OptionB = 1 << 1,
+	OptionC = 1 << 2,
+	OptionD = 1 << 3,
+	OptionE = 1 << 4,
 };
 
 int main()
 {
-    // Combine flags using bitwise OR.
-    SomeFlag bcd = SomeFlag::OptionB | SomeFlag::OptionC | SomeFlag::OptionD;
-    std::cout << "Initial flags set: OptionB, OptionC, OptionD.\n";
+	// Combine flags using bitwise OR.
+	SomeFlag bcd = SomeFlag::OptionB | SomeFlag::OptionC | SomeFlag::OptionD;
+	std::cout << "Initial flags set: OptionB, OptionC, OptionD.\n";
 
-    // Check if OptionA is present using bitwise AND.
-    if ((bcd & SomeFlag::OptionA) == SomeFlag::OptionA)
-    {
-        std::cout << "OptionA is set.\n";
-    }
+	// Check if OptionA is present using bitwise AND.
+	if ((bcd & SomeFlag::OptionA) == SomeFlag::OptionA)
+	{
+		std::cout << "OptionA is set.\n";
+	}
 
-    // Check if OptionB is present using bitwise AND.
-    if ((bcd & SomeFlag::OptionB) == SomeFlag::OptionB)
-    {
-        std::cout << "OptionB is set.\n";
-    }
+	// Check if OptionB is present using bitwise AND.
+	if ((bcd & SomeFlag::OptionB) == SomeFlag::OptionB)
+	{
+		std::cout << "OptionB is set.\n";
+	}
 
-    // Check if OptionD OR OptionE is present using special &&.
-    if (bcd && (SomeFlag::OptionD | SomeFlag::OptionE))
-    {
-        std::cout << "OptionD or OptionE is set.\n";
-    }
+	// Check if OptionD OR OptionE is present using special &&.
+	if (bcd && (SomeFlag::OptionD | SomeFlag::OptionE))
+	{
+		std::cout << "OptionD or OptionE is set.\n";
+	}
 
-    // Remove OptionB.
-    bcd &= ~SomeFlag::OptionB;
-    std::cout << "Removed OptionB.\n";
-    if ((bcd & SomeFlag::OptionB) == SomeFlag::OptionB)
-    {
-        std::cout << "OptionB is set (unexpected!).\n";
-    }
-    else
-    {
-        std::cout << "OptionB is not set.\n";
-    }
+	// Remove OptionB.
+	bcd &= ~SomeFlag::OptionB;
+	std::cout << "Removed OptionB.\n";
+	if ((bcd & SomeFlag::OptionB) == SomeFlag::OptionB)
+	{
+		std::cout << "OptionB is set (unexpected!).\n";
+	}
+	else
+	{
+		std::cout << "OptionB is not set.\n";
+	}
 
-    // Add OptionA.
-    bcd |= SomeFlag::OptionA;
-    std::cout << "Added OptionA.\n";
-    if ((bcd & SomeFlag::OptionA) == SomeFlag::OptionA)
-    {
-        std::cout << "OptionA is set.\n";
-    }
+	// Add OptionA.
+	bcd |= SomeFlag::OptionA;
+	std::cout << "Added OptionA.\n";
+	if ((bcd & SomeFlag::OptionA) == SomeFlag::OptionA)
+	{
+		std::cout << "OptionA is set.\n";
+	}
 }
--- tests/enum_operators_tests.cpp
+++ tests/enum_operators_tests.cpp
@@ -3,61 +3,61 @@
 
 enum class test_enum : int
 {
-    Zero, One, Two, Three,
-    Four, Five, Max = ~int()
+	Zero, One, Two, Three,
+	Four, Five, Max = ~int()
 };
 
 CREATE_TEST(enum_operators__arithmetic)
 {
-    using enum test_enum;
+	using enum test_enum;
 
-    CHECK((Zero | One) == One);
-    CHECK((Three & Two) == Two);
-    CHECK((Four ^ Five) == One);
-    CHECK(~Zero == Max);
-    CHECK(Two + 2 == Four);
-    CHECK(Five - 2 == Three);
+	CHECK((Zero | One) == One);
+	CHECK((Three & Two) == Two);
+	CHECK((Four ^ Five) == One);
+	CHECK(~Zero == Max);
+	CHECK(Two + 2 == Four);
+	CHECK(Five - 2 == Three);
 }
 
 CREATE_TEST(enum_operators__assignment)
 {
-    using enum test_enum;
+	using enum test_enum;
 
-    test_enum a = One;
-    a |= Two;
-    CHECK(a == Three);
+	test_enum a = One;
+	a |= Two;
+	CHECK(a == Three);
 
-    a &= Two;
-    CHECK(a == Two);
+	a &= Two;
+	CHECK(a == Two);
 
-    a ^= Two;
-    CHECK(a == Zero);
+	a ^= Two;
+	CHECK(a == Zero);
 
-    a += 3;
-    CHECK(a == Three);
+	a += 3;
+	CHECK(a == Three);
 
-    a -= 1;
-    CHECK(a == Two);
+	a -= 1;
+	CHECK(a == Two);
 }
 
 CREATE_TEST(enum_operators__increment_decrement)
 {
-    using enum test_enum;
+	using enum test_enum;
 
-    test_enum a = Two;
-    CHECK(++a == Three);
-    CHECK(a++ == Three);
-    CHECK(a == Four);
-    CHECK(--a == Three);
-    CHECK(a-- == Three);
-    CHECK(a == Two);
+	test_enum a = Two;
+	CHECK(++a == Three);
+	CHECK(a++ == Three);
+	CHECK(a == Four);
+	CHECK(--a == Three);
+	CHECK(a-- == Three);
+	CHECK(a == Two);
 }
 
 CREATE_TEST(enum_operators__special)
 {
-    using enum test_enum;
+	using enum test_enum;
 
-    CHECK((Five - Two) == 3);
-    CHECK(Three && Two);
-    CHECK(not (Four && Zero));
+	CHECK((Five - Two) == 3);
+	CHECK(Three && Two);
+	CHECK(not (Four && Zero));
 }
